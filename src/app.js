7512928459f5dd6f284fcbd121d870b737251ca4
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-09-30 11:35:50 +0300

    configure morgan logging package

--- src/app.js
+++ src/app.js
@@ -2,8 +2,16 @@
 const express = require("express");
 const app = express();
 
+const morgan = require("morgan");
+
 // Essential Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
 
 module.exports = app;
