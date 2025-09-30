fbbccd84c86dd1fff263399b12351cee67fa687b
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-09-30 11:24:56 +0300

    configure dotnev package

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const app = require("./app");
 
-const PORT = 3000;
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "./src/config.env" });
+}
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`listening to requests on port ${PORT}`);
 });
