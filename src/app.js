a77402f29dbc738e710567f1e69ca43c34581b6e
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-09-30 12:05:25 +0300

    add global error handler

--- src/app.js
+++ src/app.js
@@ -10,8 +10,27 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.send("test");
+});
+
+app.use((err, req, res, next) => {
+  console.error("Error:", err.stack);
+
+  if (err.isOperational) {
+    const { statusCode = 500, message = "Something went wrong" } = err;
+
+    res.status(statusCode).json({
+      status: "error",
+      message: message,
+      stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+    });
+  }
+
+  res.status(500).json({
+    status: "error",
+    message: "Something went Wrong!",
+  });
 });
 
 module.exports = app;
