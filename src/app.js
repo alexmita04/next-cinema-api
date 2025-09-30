06fc9079d6e601ca38670df6c9030540a79aa612
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-09-30 11:30:53 +0300

    add body parsers middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+// Express App Object
 const express = require("express");
-
 const app = express();
+
+// Essential Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 module.exports = app;
