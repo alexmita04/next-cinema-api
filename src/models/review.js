981eb6791f0d828bda9e10edaa6101ac85ff4ab6
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-10-01 09:55:28 +0300

    configure review schema

---
+++ src/models/review.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema({
+  author: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "The review author is mandatory and cannot be empty."],
+  },
+  movie: {
+    type: mongoose.Types.ObjectId,
+    ref: "Movie",
+    required: [true, "The review movie is mandatory and cannot be empty."],
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: [true, "The review rating is mandatory and cannot be empty."],
+  },
+  body: {
+    type: String,
+    required: [true, "The review body is mandatory and cannot be empty."],
+  },
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
