9e97c86731149b21d5dd028065879707713b85d7
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-10-01 09:09:11 +0300

    configure cinema schema

---
+++ src/models/cinema.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const cinemaSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "The cinema name is mandatory and cannot be empty."],
+    trim: true,
+  },
+  location: {
+    type: String,
+    required: [true, "The cinema location is mandatory and cannot be empty."],
+    trim: true,
+  },
+  admin: {
+    type: mongoose.Types.ObjectId,
+    ref: "Admin",
+    required: [true, "The cinema admin is mandatory and cannot be empty."],
+  },
+  auditoriums: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Auditorium",
+      required: [
+        true,
+        "The cinema auditoriums are mandatory and cannot be empty.",
+      ],
+      validate: {
+        validator: function (auditoriumsArr) {
+          return (
+            auditoriumsArr.length === new Set(auditoriumsArr.map(String)).size
+          );
+        },
+      },
+    },
+  ],
+  openingHour: {
+    type: Date,
+    required: [
+      true,
+      "The cinema opening hour is mandatory and cannot be empty.",
+    ],
+  },
+  closingHour: {
+    type: Date,
+    required: [
+      true,
+      "The cinema closing hour is mandatory and cannot be empty.",
+    ],
+    validate: {
+      validator: function (hour) {
+        return hour > this.openingHour;
+      },
+      message: "Closing hour must be later than opening hour",
+    },
+  },
+  email: {
+    type: String,
+    required: [true, "The cinema email is mandatory and cannot be empty."],
+    unique: true,
+  },
+  parking: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Cinema = mongoose.model("Cinema", cinemaSchema);
+
+module.exports = Cinema;
