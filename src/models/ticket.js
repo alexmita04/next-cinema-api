c1a106d0023eceda0a7d7b9ab1b581630c3b6bc5
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-10-01 09:16:23 +0300

    configure ticket schema

---
+++ src/models/ticket.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ticketSchema = new Schema(
+  {
+    screening: {
+      type: mongoose.Types.ObjectId,
+      ref: "Screening",
+      required: [
+        true,
+        "The ticket screening is mandatory and cannot be empty.",
+      ],
+    },
+    customer: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "The ticket customer is mandatory and cannot be empty."],
+    },
+    seat: {
+      row: {
+        type: Number,
+        required: [
+          true,
+          "The ticket seat (row) is mandatory and cannot be empty.",
+        ],
+      },
+      number: {
+        type: Number,
+        required: [
+          true,
+          "The ticket seat (number) is mandatory and cannot be empty.",
+        ],
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+ticketSchema.index(
+  {
+    screening: 1,
+    "seat.row": 1,
+    "seat.number": 1,
+  },
+  {
+    unique: true,
+  }
+);
+
+const Ticket = mongoose.model("Ticket", ticketSchema);
+
+module.exports = Ticket;
