acfa4c535a56775589b714464fbf4d0cbfc32cf0
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-10-01 09:39:13 +0300

    configure admin schema

---
+++ src/models/admin.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const adminSchema = new Schema(
+  {
+    cinema: {
+      type: mongoose.Types.ObjectId,
+      ref: "Cinema",
+      required: [true, "The admin cinema is mandatory and cannot be empty."],
+    },
+    username: {
+      type: String,
+      required: [true, "The admin username is mandatory and cannot be empty."],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "The admin password is mandatory and cannot be empty."],
+    },
+    email: {
+      type: String,
+      required: [true, "The admin email is mandatory and cannot be empty."],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;
