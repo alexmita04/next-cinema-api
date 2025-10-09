e60bcebcf8d02a2cbc6ed842a883d28d9aa2d4d8
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-10-09 08:43:22 +0300

    refactor: add db uri as a env var

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.bzrhs0z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = process.env.DB_URI;
 
 mongoose
   .connect(uri)
